c7dc1fbde47e55e21ccbd810053dfee1ffac5e84
Corporation <macintosh1mb@yahoo.com>
2025-12-11 23:19:19 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), '');
-  
-  return {
+    return {
     plugins: [react()],
     define: {
       // Polyfill process.env for the client-side usage in geminiService
